f3e1d8caa70829ab3167632905bccaed0b1fb3cd
clarkzjw <clark.zhao@daocloud.io>
2018-01-02 21:59:04 +0800

    924

--- bot.py
+++ bot.py
@@ -1,89 +1,48 @@
+import time
+
+import mss
 import pyautogui as pg
-from time import sleep
+from keyboard import press_and_release
+from numpy import array, uint8
 
 
-def move(_pos, _now):
-    if _pos == 'left' and _now == 'left':
-        pg.typewrite(['left'])
-        pg.typewrite(['left'])
-    elif _pos == 'left' and _now == 'right':
-        pg.typewrite(['right'])
-        pg.typewrite(['right'])
-    elif _pos == 'right' and _now == 'right':
-        pg.typewrite(['right'])
-        pg.typewrite(['right'])
-    elif _pos == 'right' and _now == 'left':
-        pg.typewrite(['left'])
-        pg.typewrite(['left'])
+def begin():
+    pg.click(440, 900, 2)
 
-width, height = pg.size()
-width *= 0.5
-pg.moveTo(333, 600)
-pg.click()
 
-posY = [360, 260, 160]
-posX = [276, 398]
+TREE = uint8([56, 116, 161])
 
-q = ['left', 'left', 'left']
+# from left to right
+posY = [360, 460]
+# from bottom to up
+posX = [640, 540, 440, 340, 240, 140]
 
-im = pg.screenshot(region=(0, 0, width, height))
-l0 = im.getpixel((posX[0], posY[0]))
-l1 = im.getpixel((posX[0], posY[1]))
-l2 = im.getpixel((posX[0], posY[2]))
 
-r0 = im.getpixel((posX[1], posY[0]))
-r1 = im.getpixel((posX[1], posY[1]))
-r2 = im.getpixel((posX[1], posY[2]))
+def is_tree(c1):
+    if c1[0] == TREE[0] and c1[1] == TREE[1] and c1[2] == TREE[2]:
+        return True
+    else:
+        return False
 
-if l0[0] == 161 and l0[1] == 116 and l0[2] == 56:
-    q[0] = 'right'
-    pos = 'right'
-else:
-    pos = 'left'
-if l1[0] == 161 and l1[1] == 116 and l1[2] == 56:
-    q[1] = 'right'
-if l2[0] == 161 and l2[1] == 116 and l2[2] == 56:
-    q[2] = 'right'
 
-qlen = 3
-
-while True:
-    if qlen == 3:
-        now = q[0]
-        move(pos, now)
-        now = q[1]
-        move(pos, now)
-        now = q[2]
-        move(pos, now)
-        qlen = 0
-    elif qlen == 0:
-        im = pg.screenshot(region=(0, 0, width, height))
-        l0 = im.getpixel((posX[0], posY[0]))
-        l1 = im.getpixel((posX[0], posY[1]))
-        l2 = im.getpixel((posX[0], posY[2]))
-
-        r0 = im.getpixel((posX[1], posY[0]))
-        r1 = im.getpixel((posX[1], posY[1]))
-        r2 = im.getpixel((posX[1], posY[2]))
-
-        if l0[0] == 161 and l0[1] == 116 and l0[2] == 56:
-            q[0] = 'right'
+def parse_screen(img, moves):
+    left = [img[x, posY[0]] for x in posX]
+    for i in range(6):
+        if is_tree(left[i]):
+            moves.append("right, right")
         else:
-            q[0] = 'left'
-        if l1[0] == 161 and l1[1] == 116 and l1[2] == 56:
-            q[1] = 'right'
-        else:
-            q[1] = 'left'
-        if l2[0] == 161 and l2[1] == 116 and l2[2] == 56:
-            q[2] = 'right'
-        else:
-            q[2] = 'left'
+            moves.append("left, left")
 
-        now = q[0]
-        move(pos, now)
-        now = q[1]
-        move(pos, now)
-        now = q[2]
-        move(pos, now)
 
-    sleep(0.095)
+with mss.mss() as sct:
+    screen = sct.monitors[1]
+    screen["width"] = screen["width"] / 2.0
+    begin()
+
+    while True:
+        img = array(sct.grab(screen))
+        moves = list()
+        parse_screen(img, moves)
+        for m in moves:
+            press_and_release(m)
+        time.sleep(0.145)
